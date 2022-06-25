ed534c1f0d1451ae6362f35d9c9aae27174c3500
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-06-25 12:18:42 +0530

    Add files via upload

---
+++ Day 1 - Band Name Generator/BandName.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name generator.")
+city = input("Which city were you born in?\n")
+pet = input("What was the name of your pet?\n")
+print("Your band name could be",city,pet)
