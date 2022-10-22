32537b94ce63cb113f0cdb74d80a2543b5ca3e8c
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-10-22 13:54:37 +0530

    Updated High Score functionality

--- Day 20and21 - Snake Game/scoreboard.py
+++ Day 20and21 - Snake Game/scoreboard.py
@@ -1,30 +1,31 @@
 from turtle import Turtle
 
+with open("data.txt", "r") as file:
+    HIGHSCORE = file.read()
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super(Scoreboard, self).__init__()
         self.score = 0
-        self.high_score = 0
         self.penup()
         self.goto(0, 260)
         self.color("white")
         self.hideturtle()
 
     def reset_scoreboard(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > int(HIGHSCORE):
+            with open("data.txt", "w") as file:
+                file.write(str(self.score))
         self.score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
+        with open("data.txt", "r") as file:
+            HIGHSCORE = file.read()
+        self.write(f"Score: {self.score} High Score: {HIGHSCORE}", align="center", font=("Courier", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-
-    # def game_over(self):
-    #     self.goto(0,0)
-    #     self.write("Game Over.", align="center", font=("Arial", 24, "normal"))
