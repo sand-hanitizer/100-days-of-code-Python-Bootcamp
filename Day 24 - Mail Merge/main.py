365bbc8a3047977a27873cae671f3cf3648bbe94
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-10-23 10:01:58 +0530

    Completed mail merge

--- Day 24 - Mail Merge/main.py
+++ Day 24 - Mail Merge/main.py
@@ -3,15 +3,19 @@
 with open("Input/Names/invited_names.txt") as names:
     names_list = names.readlines()
 
-print(names_list)
 f_list = []
 for name in names_list:
     x = name.strip('\n')
     f_list.append(x)
-print(f_list)
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
 
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# Replace the [name] placeholder with the actual name.
+with open("Input/Letters/starting_letter.txt","r") as letter:
+    content = letter.read()
+
+# Save the letters in the folder "ReadyToSend".
+for name in f_list:
+    with open(f"Output/ReadyToSend/letter_to_{name}","w") as file:
+        letter = content
+        letter = letter.replace("[name]",f"{name}")
+        file.write(letter)
