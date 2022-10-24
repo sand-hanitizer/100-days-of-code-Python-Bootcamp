010b400c16ab0ceef4dd29882022b1fe5c2a4b28
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-10-24 11:23:36 +0530

    Update main.py

--- Day 25 - US States Name Game/Game/main.py
+++ Day 25 - US States Name Game/Game/main.py
@@ -10,17 +10,21 @@ turtle.shape("blank_states_img.gif")
 
 states_data = pd.DataFrame(pd.read_csv("50_states.csv"))
 states_name = states_data["state"].tolist()
-game_is_on = True
 guessed_states = []
 
-while game_is_on:
+while len(guessed_states) < 50:
     answer = str(screen.textinput(title=f"Guess the State,{len(guessed_states)}/50", prompt="What's the state's name?"))
     answer = string.capwords(answer)
 
-    if answer == "End" or len(guessed_states) == 50:
-        game_is_on = False
-        screen.exitonclick()
-    
+    if answer == "End":
+        missing = []
+        for x in states_name:
+            if x not in guessed_states:
+                missing.append(x)
+        data = pd.DataFrame(missing)
+        data.to_csv("states_missed.csv")
+        break
+
     text = turtle.Turtle()
     for x in states_name:
         if answer == x:
@@ -34,5 +38,3 @@ while game_is_on:
             text.goto(location)
             text.write(f"{answer}", font=("Arial", 12, "normal"))
 
-
-turtle.mainloop()
