ef04aee6724121d348079315e4509822382c799a
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-10-24 10:24:21 +0530

    Update main.py

--- Day 25 - US States Name Game/Game/main.py
+++ Day 25 - US States Name Game/Game/main.py
@@ -6,6 +6,6 @@ screen.title("US States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
-answer = screen.textinput(title="Guess the State",prompt="What's the state's name?")
+answer = screen.textinput(title="Guess the State", prompt="What's the state's name?")
 
 turtle.mainloop()
