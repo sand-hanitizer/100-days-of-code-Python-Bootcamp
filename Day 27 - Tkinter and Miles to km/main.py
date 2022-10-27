56a606a7e28e498e9025d8f571a1801d1b314442
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-10-27 23:11:26 +0530

    Update main.py

--- Day 27 - Tkinter and Miles to km/main.py
+++ Day 27 - Tkinter and Miles to km/main.py
@@ -9,28 +9,26 @@ def miles_to_km():
 
 window = Tk()
 window.title("Miles to Kilometer Converter")
-window.config(padx=20, pady=20, width=200, height=200)
+window.minsize(height=300, width=300)
+window.config(padx=20, pady=20)
 
-empty_label = Label(text="None", fg="#f0f0f0")
-empty_label.grid(column=0, row=0)
 
 miles_input = Entry(width=7)
 miles_input.grid(column=1, row=0)
 
-miles_label = Label(text="Miles",font=("Helvetica",16,"italic"))
-miles_input.grid(column=2, row=0)
+miles_label = Label(text="Miles",font=("Arial",16,"bold"))
+miles_label.grid(column=2, row=0)
 
-is_equal_label = Label(text="is equal to",font=("Helvetica",16,"italic"))
-miles_input.grid(column=0, row=1)
+is_equal_label = Label(text="is equal to", font=("Helvetica", 16, "italic"))
+is_equal_label.grid(column=0, row=1)
 
-km_result_label = Label(text="0",font=("Helvetica",16,"italic"))
-miles_input.grid(column=1, row=1)
+km_result_label = Label(text="0", font=("Helvetica", 16, "italic"))
+km_result_label.grid(column=1, row=1)
 
-km_label = Label(text="km",font=("Helvetica",16,"italic"))
-miles_input.grid(column=2, row=1)
+km_label = Label(text="km", font=("Helvetica", 16, "italic"))
+km_label.grid(column=2, row=1)
 
-calculate_button = Button(text="Calculate", command=miles_to_km,font=("Helvetica",16,"italic"))
-miles_input.grid(column=1, row=2)
-
+calculate_button = Button(text="Calculate", command=miles_to_km, font=("Helvetica", 16, "italic"))
+calculate_button.grid(column=1, row=2)
 
 window.mainloop()
