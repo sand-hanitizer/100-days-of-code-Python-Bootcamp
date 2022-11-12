e89818fc4b73ef52543410b6c6e7dcf38e0d89be
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-11-12 22:59:45 +0530

    update main.py

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,27 +1,51 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+
+import pyperclip
+from pyperclip import copy, paste
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_password():
+    password_entry.delete(0, END)
+    password_list = []
+
+    password_list += [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, f"{password}")
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     is_ok = False
     website = website_entry.get()
     user = username_entry.get()
-    password = password_entry.get()
-    if len(website) == 0 or len(password) == 0:
+    pass_word = password_entry.get()
+    if len(website) == 0 or len(pass_word) == 0:
         messagebox.showwarning(title="Warning", message="Do not leave the fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered"
-                                                      f"\nEmail : {user}"
-                                                      f"\nPassword : {password}"
-                                                      f"\nSave?")
+                                                              f"\nEmail : {user}"
+                                                              f"\nPassword : {pass_word}"
+                                                              f"\nSave?")
     if is_ok:
         with open("data.txt", "a") as file:
-            file.write(f"{website} | {user} | {password} \n")
+            file.write(f"{website} | {user} | {pass_word} \n")
             website_entry.delete(0, END)
-            username_entry.delete(0, END)
             password_entry.delete(0, END)
 
 
@@ -58,7 +82,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=20)
 password_entry.grid(row=3, column=1, sticky="EW")
 
-gen_pass = Button(text="Generate Password", highlightthickness=0, bg="white", fg="#024B30")
+gen_pass = Button(text="Generate Password", highlightthickness=0, bg="white", fg="#024B30", command=generate_password)
 gen_pass.grid(row=3, column=2, sticky="EW")
 
 # add button
