9426e38fa0f7d286f2c00c92516dcff19d9ed435
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-11-12 10:47:17 +0530

    Update main.py

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,5 +1,21 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+# display
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# password manager logo
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=img)
+canvas.grid(row=1,column=1)
+
+# keep screen on
+window.mainloop()
