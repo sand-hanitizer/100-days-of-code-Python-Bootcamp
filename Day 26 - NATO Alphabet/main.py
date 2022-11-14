5ebbcbf895bbaa2a067bad4329167d0e89ce8881
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-11-14 11:45:09 +0530

    Update main.py

--- Day 26 - NATO Alphabet/main.py
+++ Day 26 - NATO Alphabet/main.py
@@ -8,5 +8,10 @@ nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 name = list(input("Enter your name: ").upper())
-list_name = [nato_dict[x] for x in name]
-print(list_name)
+try:
+    list_name = [nato_dict[x] for x in name]
+except KeyError:
+    print("Sorry, only letters are accepted.")
+    name = list(input("Enter your name: ").upper())
+else:
+    print(list_name)
