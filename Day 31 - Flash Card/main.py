fcead50b3cadef5041b8849a12e7ffe07e3504bf
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-12-18 23:03:41 +0530

    update

--- Day 31 - Flash Card/main.py
+++ Day 31 - Flash Card/main.py
@@ -1,8 +1,23 @@
 from tkinter import *
+from pandas import DataFrame, read_csv
+from random import choice
 
-word = "WORD"
-meaning = "MEANING"
 BACKGROUND_COLOR = "#B1DDC6"
+
+title = "French"
+word = "WORD"
+
+# --------------------------- READ RANDOM FRENCH WORDS --------------------------------
+with open("data/french_words.csv") as file:
+    data = read_csv(file)
+    french_words = DataFrame.to_dict(data, orient="records")
+    print(french_words)
+
+
+def get_word():
+    word_dict = choice(french_words)
+    french_word = word_dict["French"]
+    meaning = word_dict["English"]
 
 
 # -------------------------- UI FOR THE FLASH CARD -------------------------------------
@@ -20,8 +35,8 @@ wrong = PhotoImage(file="images/wrong.png")
 display_front = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 display_front.create_image(400, 263, image=flashcard_front)
 display_front.grid(row=0, column=0, columnspan=2)
-display_front.create_text(400,150,font="Arial 40 italic",text=f"{word}")
-display_front.create_text(400,263,font="Arial 60 bold",text=f"{meaning}")
+display_front.create_text(400, 150, font="Arial 40 italic", text=f"{title}")
+display_front.create_text(400, 263, font="Arial 60 bold", text=f"{word}")
 
 # Buttons
 correct = Button(image=right, highlightthickness=0, bg=BACKGROUND_COLOR)
