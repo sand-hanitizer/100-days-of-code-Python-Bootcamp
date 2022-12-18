2c339e2876600780732b50b6c543a052efcbf46b
sand-hanitizer <95733894+sand-hanitizer@users.noreply.github.com>
2022-12-18 22:35:01 +0530

    Update main.py

--- Day 31 - Flash Card/main.py
+++ Day 31 - Flash Card/main.py
@@ -1,4 +1,28 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 
+# -------------------------- UI FOR THE FLASH CARD -------------------------------------
 
+window = Tk()
+window.title("Flash Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Images
+flashcard_front = PhotoImage(file="images/card_front.png")
+right = PhotoImage(file="images/right.png")
+wrong = PhotoImage(file="images/wrong.png")
+
+# Canvas
+display_front = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
+display_front.create_image(400, 266, image=flashcard_front)
+display_front.grid(row=0, column=0, columnspan=2)
+
+# Buttons
+correct = Button(image=right, highlightthickness=0, bg=BACKGROUND_COLOR)
+correct.grid(row=1, column=0)
+incorrect = Button(image=wrong, highlightthickness=0, bg=BACKGROUND_COLOR)
+incorrect.grid(row=1, column=1)
+
+window.mainloop()
